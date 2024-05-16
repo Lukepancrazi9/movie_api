9eb36e758bc4c3802887f75d3c8e9f407893cbc0
Lukepancrazi9 <lukepancrazi@gmail.com>
2024-05-16 14:58:08 -0700

    Mongoose 2.8 API Update
    Installed Mongoose, incorporated models, updated endpoints, and updated documentation

--- index.js
+++ index.js
@@ -1,217 +1,156 @@
-const express = require('express'),
-  morgan = require('morgan'),
-  bodyParser = require('body-parser'),
-  uuid = require('uuid');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Models = require('./models.js');  
 const app = express();
- 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan('common'));
 
-app.use(bodyParser.json());
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
 
-let users = [
+mongoose.connect('mongodb://localhost:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//User Info
+//Add User
+/* Expect JSON in this Format
+{
+    ID: Integer
+    Username: String,
+    Password: String,
+    Email: String,
+    Birthday: Date
+}*/
+app.post('/users', async (req, res) => {
+    await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+        if (user) {
+            return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+            Users
+            .create({
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+            .catch((error) => {
+                console.error(error);
+                res.status(500).send('Error: ' + error);
+            })
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    });
+});
+
+//Read All Users
+app.get('/users', async (req, res) => {
+    await Users.find()
+    .then((users) => {
+        res.status(201).json(users);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+// Read A User by Username
+app.get('/users/:Username', async (req, res) => {
+    await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+        res.json(user);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+//Update User Info by Username
+/* Expect JSON in this format
+{
+    Username: String,
+    (required)
+    Password: String,
+    (required)
+    Email: String,
+    (required)
+    Birthday: Date
+}*/
+app.put('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
-        id: 1,
-        name: 'Scooby',
-        topMovies: []
-    },
-    {
-        id: 2,
-        name: 'Shaggy',
-        topMovies: ['The Dark Knight']
+        Username: req.bodyUsername,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
     }
-]
+  },
+  { new:true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  })
+});
 
-let topTenMovies = [
-    {
-        title: 'Saving Private Ryan',
-        year: '1998',
-        genre: {
-            genreType: 'War',
-            genreDescription: 'War is a film genre concerned with warfare, typically about naval, air, or land battles, with combat scenes central to the drama.'
-        },
-        director: {
-            directorName:'Steven Spielberg',
-            directorBirth: 'December 18, 1946'
-        }
+//Add a Movie to User's Favorites
+app.post('/users/:Username/:movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $push: { Favorites: req.params.MovieID }
     },
-    {
-        title: 'Star Wars: Revenge of the Sith',
-        year: '2005',
-        genre: {
-            genreType: 'Sci-fi',
-            genreDescription: 'Science fiction is a genre of speculative fiction, which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life.'
-        },
-        director: {
-            directorName: 'George Lucas',
-            directorBirth: 'May 14, 1944'
-        }
+    { new: true}) //This line makes sure that the updated document is returned
+    .then((updatedUser) => { 
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+//Delete a Favorite for User
+app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $pull: { Favorites: req.params.MovieID } 
     },
-    {
-        title: 'Ex Machina',
-        year: '2015',
-        genre: {
-            genreType: 'Thriller',
-            genreDescription: 'Thrillers are characterized and defined by the moods they elicit, giving their audiences heightened feelings of suspense, excitement, surprise, anticipation and anxiety.'
-        },
-        director: {
-            directorName: 'Alex Garland',
-            directorBirth: 'May 26, 1970'
+    { new: true}) //This line amkes sure that the updated document is returned
+    .then((updatedUser) => {
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+//Delete User by Username
+app.delete('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username })
+    .then((user) => {
+        if (!user) {
+            res.status(400).send(req.params.Username + ' was not found');
+        } else {
+            res.status(200).send(req.params.Username + ' was deleted.');
         }
-    },
-    {
-        title: 'The Dark Knight',
-        year: '2008',
-        genre: {
-            genreType: 'Action',
-            genreDescription: 'Action is a genre that focuses on stories that involve high-stakes, high-energy, and fast-paced events.'
-        },
-        director: {
-            directorName: 'Christopher Nolan',
-            directorBirth: 'July 30, 1970'
-        }
-    },
-    {
-        title: 'Lord of the Rings: The Two Towers',
-        year: '2002',
-        genre: {
-            genreType: 'Adventure',
-            genreDescription: 'Adventure is a genre of fiction in which an adventure, an exciting undertaking involving risk and physical danger, forms the main storyline.'
-        },
-        director: {
-            directorName: 'Peter Jackson',
-            directorBirth: 'October 31, 1961'
-        }
-    },
-    {
-        title: 'The Usual Suspects',
-        year: '1995',
-        genre: {
-            genreType: 'Crime',
-            genreDescription: 'Crime is a genre that centre on criminal acts and especially on the investigation, either by an amateur or a professional detective, of a crime, often a murder.'
-        },
-        director: {
-            directorName: 'Bryan Singer',
-            directorBirth: 'September 17, 1965'
-        }
-    },
-    {
-        title: 'Prisoners',
-        year: '2013',
-        genre: {
-            genreType: 'Thriller',
-            genreDescription: 'Thrillers are characterized and defined by the moods they elicit, giving their audiences heightened feelings of suspense, excitement, surprise, anticipation and anxiety.'
-        },
-        director: {
-            directorName: 'Denis Villeneuve',
-            directorBirth: 'October 3, 1967'
-        }
-    },
-    {
-        title: 'Memories of Murder',
-        year: '2003',
-        genre: {
-            genreType: 'Crime',
-            genreDescription: 'Crime is a genre that centre on criminal acts and especially on the investigation, either by an amateur or a professional detective, of a crime, often a murder.'
-        },
-        director: {
-            directorName: 'Bong Joon-ho',
-            directorBirth: 'September 14, 1969'
-        }
-    },
-    {
-        title: 'Se7en',
-        year: '1995',
-        genre: {
-            genreType: 'Mystery',
-            genreDescription: 'Mystery is a fiction genre where the nature of an event, usually a murder or other crime, remains mysterious until the end of the story.'
-        },
-        director: {
-            directorName: 'David Fincher',
-            directorBirth: 'August 28, 1962'
-        }
-    },
-    {
-        title: 'Pirates of the Caribbean: The Curse of the Black Pearl',
-        year: '2003',
-        genre: {
-            genreType: 'Adventure',
-            genreDescription: 'Adventure is a genre of fiction in which an adventure, an exciting undertaking involving risk and physical danger, forms the main storyline.'
-        },
-        director: {
-            directorName: 'Gore Verbinski',
-            directorBirth: 'March 16, 1964'
-        }
-    },
-];
-
-//Create User
-app.post('/users', (req, res) => {
-    const newUser = req.body;
-
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('Users need names.')
-    }
-})
-
-//Update User
-app.put('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedUser = req.body;
-    let user = users.find( user => user.id == id);
-
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('No User Found.')
-    }
-})
-
-//Create Top Movies for User
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const id = req.params.id;
-    const movieTitle = req.params.movieTitle;
-    let user = users.find( user => user.id == id);
-
-    if (user) {
-        user.topMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-    } else {
-        res.status(400).send('No User Found')
-    }
-})
-
-//Delete Top Movie for User
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const id = req.params.id;
-    const movieTitle = req.params.movieTitle;
-    let user = users.find( user => user.id == id);
-
-    if (user) {
-        user.topMovies = user.topMovies.filter( title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`); 
-    } else {
-        res.status(400).send('No User Found')
-    }
-})
-
-//Delete User 
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    let user = users.find( user => user.id == id);
-
-
-    if (user) {
-        users = users.filter( user => user.id != id);
-        res.status(200).send(` User ${id} has been deleted`); 
-    } else {
-        res.status(400).send('No User Found')
-    }
-})
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
 app.use(express.static('public'));
 
@@ -220,46 +159,118 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Movie API!');
 });
 
-//Read movie list 
-app.get('/movies', (req, res) => {
-    res.status(200).json(topTenMovies);
+//Info on Movies
+//Read All Movies
+app.get('/movies', async (req, res) => {
+    await Movies.find()
+    .populate('Director', 'Name Bio')
+    .populate('Genre', 'Name Description')
+    .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
-//Read movie by title
-app.get('/movies/:title', (req, res) => {
-    const title = req.params.title;
-    const movie = topTenMovies.find( movie => movie.title === title);
+// Read A Movie by Title
+app.get('/movies/:Title', async (req, res) => {
+    await Movies.findOne({ Title: req.params.Title })
+    .populate('Director', 'Name Bio')
+    .populate('Genre', 'Name Description')
+    .then((movie) => {
+        res.json(movie);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
-    if (movie) { 
-        res.status(200).json(movie);
-    } else {
-        res.staus(400).send('No Movie Found')
+// Read Movies by Genre Name
+app.get('/movies/genre/:Name', async (req, res) => {
+    try {
+        const genre = await Genres.findOne({ Name: req.params.Name });
+        if (!genre) {
+            return res.status(404).send('Genre not found');
+        }
+        const movies = await Movies.find({ Genre: genre._id })
+            .populate('Director', 'Name Bio')
+            .populate('Genre', 'Name Description');
+        res.status(200).json(movies);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
     }
-})
+});
 
-//Read Movie by Genre 
-app.get('/movies/genre/:genreType', (req, res) => {
-    const genreType = req.params.genreType;
-    const genre = topTenMovies.find( movie => movie.genre.genreType === genreType).genre;
-
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send('No Genre Found')
+// Read Movies by Director Name
+app.get('/movies/director/:Name', async (req, res) => {
+    try {
+        const director = await Directors.findOne({ Name: req.params.Name });
+        if (!director) {
+            return res.status(404).send('Director not found');
+        }
+        const movies = await Movies.find({ Director: director._id })
+            .populate('Director', 'Name Bio')
+            .populate('Genre', 'Name Description');
+        res.status(200).json(movies);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
     }
-})
+});
 
-//Read Movie by Director
-app.get('/movies/directors/:directorName', (req, res) => {
-    const directorName = req.params.directorName;
-    const director = topTenMovies.find( movie => movie.director.directorName === directorName).director;
+//Info on Genres
+//Read All Genres
+app.get('/genres', async (req, res) => {
+    await Genres.find()
+    .then((genres) => {
+        res.status(201).json(genres);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send('No Director Found')
-    }
-})
+// Read A Genre by Name
+app.get('/genres/:Name', async (req, res) => {
+    await Genres.findOne({ Name: req.params.Name })
+    .then((genre) => {
+        res.json(genre);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+//Info on Directors
+//Read All Directors
+app.get('/directors', async (req, res) => {
+    await Directors.find()
+    .then((directors) => {
+        res.status(201).json(directors);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+// Read A Director by Name
+app.get('/directors/:Name', async (req, res) => {
+    await Directors.findOne({ Name: req.params.Name })
+    .then((director) => {
+        res.json(director);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
 app.get('/secreturl', (req, res) => {
     res.send('This is a secret url with super top-secret content.');
