aa96020b8f5f7b132a89792db454f2ab20cb2518
Lukepancrazi9 <lukepancrazi@gmail.com>
2024-05-31 12:55:08 -0700

    mongoose connect

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect( process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //User Info
 //Add User
