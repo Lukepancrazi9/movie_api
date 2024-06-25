a943f4b686112635385cde823cd0b18ebaa7b78e
Lukepancrazi9 <lukepancrazi@gmail.com>
2024-06-25 14:01:48 -0700

    updated cors allowedOrigins

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan('common'));
 app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
