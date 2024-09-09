8f4d6bf685c35897ed3181adda8849e04560c2f8
Lukepancrazi9 <lukepancrazi@gmail.com>
2024-09-09 13:27:03 -0700

    alllowed origins update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan('common'));
 app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://crazimovies.netlify.app'];
 
 app.use(cors({
     origin: (origin, callback) => {
